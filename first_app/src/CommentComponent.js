3175a3e854b7855e4da7357e489b93ea8fb9b4e1
Ketan <ketantoo@gmail.com>
2020-01-02 21:33:06 +0530

    using faker package to generate fake comment component

---
+++ first_app/src/CommentComponent.js
@@ -0,0 +1,24 @@
+import React from "react";
+import faker from "faker";
+const CommentComponent = () => {
+  return (
+    <div className="ui container comments">
+      <div className="comment">
+        <a href="/" className="avatar">
+          <img alt="avatar" src={faker.image.avatar()} />
+        </a>
+        <div className="content">
+          <a href="/" className="author">
+            {faker.name.findName()}
+          </a>
+          <div className="metadata">
+            <span className="date"> {faker.date.weekday()}</span>
+          </div>
+          <div className="text">{faker.random.words()}</div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CommentComponent;
