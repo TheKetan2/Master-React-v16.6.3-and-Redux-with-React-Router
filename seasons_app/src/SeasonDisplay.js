a7b97432c7e115631afc68acc739eeb1e1a73d0f
Ketan <ketantoo@gmail.com>
2020-01-04 14:33:31 +0530

    refractored season data with seasonConfig object

--- seasons_app/src/SeasonDisplay.js
+++ seasons_app/src/SeasonDisplay.js
@@ -1,7 +1,37 @@
 import React from "react";
+import Summer from "./img/summer.jpg";
+import Winter from "./img/winter.jpg";
 
-const SeasonDisplay = () => {
-  return <div>Season SeasonDisplay</div>;
+const SeasonConfig = {
+  summer: {
+    text: "Let's hit the beach",
+    iconName: "sun"
+  },
+  winter: {
+    text: "Burr, it's chilly!",
+    iconName: "snowflake"
+  }
+};
+
+const getSeason = (lat, month) => {
+  if (month > 2 && month < 9) {
+    return lat > 0 ? "summer" : "winter";
+  } else {
+    return lat > 0 ? "winter" : "summer";
+  }
+};
+
+const SeasonDisplay = props => {
+  const season = getSeason(props.lat, new Date().getMonth());
+
+  const seasonConfig = SeasonConfig[season];
+  return (
+    <div>
+      <i className={`${seasonConfig.iconName} icon`} />
+      <h1>{seasonConfig.text}</h1>
+      <i className={`${seasonConfig.iconName} icon`} />
+    </div>
+  );
 };
 
 export default SeasonDisplay;
--- seasons_app/src/index.js
+++ seasons_app/src/index.js
@@ -3,12 +3,17 @@ import ReactDOM from "react-dom";
 import SeasonDisplay from "./SeasonDisplay";
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      lat: null,
-      errorMsg: ""
-    };
+  // constructor(props) {
+  //   super(props);
+  //   // this.state = {
+  //   //   lat: null,
+  //   //   errorMsg: ""
+  //   // };
+  // }
+
+  state = { lat: null, errorMsg: "" };
+
+  componentDidMount() {
     window.navigator.geolocation.getCurrentPosition(
       position => {
         this.setState({ lat: position.coords.latitude });
@@ -20,12 +25,12 @@ class App extends React.Component {
   render() {
     return (
       <div>
-        {!this.state.errorMsg && !this.state.lat
+        {/* {!this.state.errorMsg && !this.state.lat
           ? "Loading..."
           : this.state.errorMsg
           ? `Error: ${this.state.errorMsg}`
-          : `Latitude: ${this.state.lat}`}
-        <SeasonDisplay />
+          : `Latitude: ${this.state.lat}`} */}
+        <SeasonDisplay lat={this.state.lat} />
       </div>
     );
   }
