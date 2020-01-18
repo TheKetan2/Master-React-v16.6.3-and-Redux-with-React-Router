8530e8688b4c37dfb79aa95d89779e762441f42d
Ketan <ketantoo@gmail.com>
2020-01-18 20:17:31 +0530

    implemented google auth with realtime auth status cheking

--- twitch_app/client/src/components/GoogleAuth.js
+++ twitch_app/client/src/components/GoogleAuth.js
@@ -1,18 +1,39 @@
 import React, { Component } from "react";
 
 class GoogleAuth extends Component {
+  state = { isSignedIn: null };
   componentDidMount() {
     window.gapi.load("client:auth2", () => {
-      window.gapi.client.init({
-        clientId:
-          "654653073494-mv1n63a9jdnbekkf43lb3oa2t3q777fb.apps.googleusercontent.com",
-        scope: "email"
-      });
+      window.gapi.client
+        .init({
+          clientId:
+            "654653073494-mv1n63a9jdnbekkf43lb3oa2t3q777fb.apps.googleusercontent.com",
+          scope: "email"
+        })
+        .then(() => {
+          this.onAuthChange();
+          this.auth.isSignedIn.listen(this.onAuthChange);
+        });
     });
   }
 
+  onAuthChange = () => {
+    this.auth = window.gapi.auth2.getAuthInstance();
+    this.setState({ isSignedIn: this.auth.isSignedIn.get() });
+  };
+
+  renderAuthButton() {
+    if (this.state.isSignedIn) {
+      return <div>Signed In</div>;
+    } else if (this.state.isSignedIn === null) {
+      return <div>Checking...</div>;
+    } else {
+      return <div>Not Signed in</div>;
+    }
+  }
+
   render() {
-    return <div>Google Auth</div>;
+    return <div>{this.renderAuthButton()}</div>;
   }
 }
 
