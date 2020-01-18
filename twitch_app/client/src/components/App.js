2550093c804297646656b7b5de36fc26c6ca7e76
Ketan <ketantoo@gmail.com>
2020-01-18 12:00:45 +0530

    always visible header

--- twitch_app/client/src/components/App.js
+++ twitch_app/client/src/components/App.js
@@ -1,21 +1,21 @@
 import React from "react";
-import { BrowserRouter, Route } from "react-router-dom";
-
-const PageOne = () => {
-  return <div>PageOne</div>;
-};
-
-const PageTwo = () => {
-  return <div>PageTwo</div>;
-};
-
+import { BrowserRouter, Route, Link } from "react-router-dom";
+import StreamList from "./streams/StreamList";
+import StreamEdit from "./streams/StreamEdit";
+import StreamCreate from "./streams/StreamCreate";
+import StreamDelete from "./streams/StreamDelete";
+import StreamShow from "./streams/StreamShow";
 const App = () => {
   return (
     <div>
       <BrowserRouter>
         <div>
-          <Route path="/" exact component={PageOne} />
-          <Route path="/two" exact component={PageTwo} />
+          <h1>Header</h1>
+          <Route path="/" exact component={StreamList} />
+          <Route path="/streams/new" component={StreamCreate} />
+          <Route path="/streams/edit" component={StreamEdit} />
+          <Route path="/streams/delete" component={StreamDelete} />
+          <Route path="/streams/show" component={StreamShow} />
         </div>
       </BrowserRouter>
     </div>
