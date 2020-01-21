7938ebc311c2998aa6410051a358ea5145306f60
Ketan <ketantoo@gmail.com>
2020-01-21 11:27:47 +0530

    customizing form input field

--- twitch_app/client/src/components/streams/StreamCreate.js
+++ twitch_app/client/src/components/streams/StreamCreate.js
@@ -3,14 +3,23 @@ import { Field, reduxForm } from "redux-form";
 
 class StreamCreate extends Component {
   renderInput(formProps) {
-    return <input {...formProps.input} />;
+    return (
+      <div className="field">
+        <label>{formProps.label}</label>
+        <input {...formProps.input} />
+      </div>
+    );
   }
   render() {
     // console.log(this.props);
     return (
-      <form>
-        <Field name="title" component={this.renderInput} />
-        <Field name="description" component={this.renderInput} />
+      <form className="ui form">
+        <Field name="title" component={this.renderInput} label="Enter Title" />
+        <Field
+          name="description"
+          component={this.renderInput}
+          label="Enter Description"
+        />
       </form>
     );
   }
