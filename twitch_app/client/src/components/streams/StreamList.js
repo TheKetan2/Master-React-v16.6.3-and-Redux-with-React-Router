2cfcddfc3065bb4c77d5a23240afae15353e84a5
Ketan <ketantoo@gmail.com>
2020-01-24 13:24:34 +0530

    wiring action reducer to stream list

--- twitch_app/client/src/components/streams/StreamList.js
+++ twitch_app/client/src/components/streams/StreamList.js
@@ -1,17 +1,23 @@
 import React from "react";
 import { connect } from "react-redux";
 // import reducers from "../../reducers";
-const StreamList = props => {
-  return (
-    <div>
-      <h2>StreamList</h2>
-      User Id: {props.userId}
-    </div>
-  );
-};
+import { fetchStreams } from "../../actions";
+class StreamList extends React.Component {
+  componentDidMount() {
+    this.props.fetchStreams();
+  }
 
-const mapStateToProps = state => {
-  return { isSignedIn: state.auth.isSignedIn, userId: state.auth.userId };
-};
+  render() {
+    return (
+      <div>
+        <h2>StreamList</h2>
+      </div>
+    );
+  }
+}
 
-export default connect(mapStateToProps)(StreamList);
+// const mapStateToProps = state => {
+//   return { isSignedIn: state.auth.isSignedIn, userId: state.auth.userId };
+// };
+
+export default connect(null, { fetchStreams })(StreamList);
