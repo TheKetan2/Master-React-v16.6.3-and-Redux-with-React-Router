3bd9c5b2384f3afeecc17d9363c8967d822a6335
Ketan <ketantoo@gmail.com>
2020-01-24 15:37:54 +0530

    created custom history object

--- twitch_app/client/src/actions/index.js
+++ twitch_app/client/src/actions/index.js
@@ -25,7 +25,9 @@ export const signOut = () => {
 export const createStream = formValues => async (dispatch, getState) => {
   const { userId } = getState().auth;
   const response = await streams.post("/streams", { ...formValues, userId });
+
   dispatch({ type: CREATE_STREAM, payload: response.data });
+  // redirect user to root route
 };
 
 export const fetchStreams = () => async dispatch => {
--- twitch_app/client/src/components/App.js
+++ twitch_app/client/src/components/App.js
@@ -1,15 +1,16 @@
 import React from "react";
-import { BrowserRouter, Route, Link } from "react-router-dom";
+import { Router, Route, Link } from "react-router-dom";
 import StreamList from "./streams/StreamList";
 import StreamEdit from "./streams/StreamEdit";
 import StreamCreate from "./streams/StreamCreate";
 import StreamDelete from "./streams/StreamDelete";
 import StreamShow from "./streams/StreamShow";
 import Header from "./Header";
+import history from "../history";
 const App = () => {
   return (
     <div>
-      <BrowserRouter>
+      <Router history={history}>
         <div>
           <Header />
           <Route path="/" exact component={StreamList} />
@@ -18,7 +19,7 @@ const App = () => {
           <Route path="/streams/delete" component={StreamDelete} />
           <Route path="/streams/show" component={StreamShow} />
         </div>
-      </BrowserRouter>
+      </Router>
     </div>
   );
 };
