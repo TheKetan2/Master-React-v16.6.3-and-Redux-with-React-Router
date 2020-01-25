b72105a70a89da2eb8c4627899454ca16c05ec12
Ketan <ketantoo@gmail.com>
2020-01-25 15:05:21 +0530

    reusable StreamForm component created

---
+++ twitch_app/client/src/components/streams/StreamForm.js
@@ -0,0 +1,67 @@
+import React, { Component } from "react";
+import { Field, reduxForm } from "redux-form";
+// import { connect } from "react-redux";
+// import { createStream } from "../../actions/index";
+class StreamForm extends Component {
+  showError = ({ touched, error }) => {
+    if (touched && error) {
+      return (
+        <div className="ui error message">
+          <div className="header">{error}</div>
+        </div>
+      );
+    }
+  };
+
+  renderInput = ({ input, label, meta }) => {
+    console.log(meta);
+    const className = `field ${meta.error && meta.touched ? "error" : null}`;
+    return (
+      <div className={className}>
+        <label>{label}</label>
+        <input {...input} autoComplete="off" />
+        {this.showError(meta)}
+      </div>
+    );
+  };
+
+  onSubmit = formValues => {
+    // event.preventDefault();
+    // console.log(formValues);
+    // this.props.createStream(formValues);
+    this.props.onSubmit(formValues);
+  };
+
+  render() {
+    // console.log(this.props);
+    return (
+      <form
+        className="ui form error"
+        onSubmit={this.props.handleSubmit(this.onSubmit)}
+      >
+        <Field name="title" component={this.renderInput} label="Enter Title" />
+        <Field
+          name="description"
+          component={this.renderInput}
+          label="Enter Description"
+        />
+        <button className="ui button primary">Submit</button>
+      </form>
+    );
+  }
+}
+
+const validate = formValues => {
+  const errors = {};
+  if (!formValues.title) {
+    errors.title = "Enter title";
+  }
+
+  if (!formValues.description) {
+    errors.description = "Enter description";
+  }
+
+  return errors;
+};
+
+export default reduxForm({ form: "streamForm", validate })(StreamForm);
