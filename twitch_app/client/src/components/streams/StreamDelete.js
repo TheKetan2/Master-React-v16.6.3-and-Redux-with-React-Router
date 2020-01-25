d40e063b462f7aa02adaa91ac1d8cc96b30c6708
Ketan <ketantoo@gmail.com>
2020-01-25 20:18:08 +0530

    making modal reusable

--- twitch_app/client/src/components/streams/StreamDelete.js
+++ twitch_app/client/src/components/streams/StreamDelete.js
@@ -2,10 +2,20 @@ import React from "react";
 import Modal from "../Modal";
 
 export default function StreamDelete() {
+  const actions = (
+    <div style={{ marginBottom: "10px" }}>
+      <buttun className="ui button negative">Delete</buttun>
+      <buttun className="ui button">Cancel</buttun>
+    </div>
+  );
   return (
     <div>
       StreamDelete
-      <Modal />
+      <Modal
+        title="Delete Stream"
+        content="Are you sure you want to delete this stream?"
+        actions={actions}
+      />
     </div>
   );
 }
