b2b0848ae83b98354ea690c3a527b01cf5619f6e
Ketan <ketantoo@gmail.com>
2020-01-26 16:00:04 +0530

    app finished

--- twitch_app/client/src/components/GoogleAuth.js
+++ twitch_app/client/src/components/GoogleAuth.js
@@ -8,7 +8,7 @@ class GoogleAuth extends Component {
     window.gapi.load("client:auth2", () => {
       window.gapi.client
         .init({
-          clientId: "",
+          clientId: "YOUR CLIENT ID",
           scope: "email"
         })
         .then(() => {
