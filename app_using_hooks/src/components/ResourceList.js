da3e4899c29760a04e3f7bf5bf8f148be0b968d4
Ketan <ketantoo@gmail.com>
2020-01-27 16:10:53 +0530

    re using hooks

--- app_using_hooks/src/components/ResourceList.js
+++ app_using_hooks/src/components/ResourceList.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 
-const ResourceList = ({ resource }) => {
+const useResources = resource => {
   const [resources, setResources] = useState([]);
 
   const fetchResource = async resource => {
@@ -15,6 +15,12 @@ const ResourceList = ({ resource }) => {
     setResources([]);
     fetchResource(resource);
   }, [resource]);
+
+  return resources;
+};
+
+const ResourceList = ({ resource }) => {
+  const resources = useResources(resource);
 
   if (!resources.length) {
     return (
