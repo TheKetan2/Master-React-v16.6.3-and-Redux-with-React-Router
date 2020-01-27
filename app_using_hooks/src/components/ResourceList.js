d57be96f8b1fdda6789d986b023ae9ea9ec3663a
Ketan <ketantoo@gmail.com>
2020-01-27 15:51:02 +0530

    used useEffect and useStae hooks for pulling data from API

--- app_using_hooks/src/components/ResourceList.js
+++ app_using_hooks/src/components/ResourceList.js
@@ -1,7 +1,53 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
-export default class ResourceList extends Component {
-  render() {
-    return <div> {this.props.resource}</div>;
+const ResourceList = ({ resource }) => {
+  const [resources, setResources] = useState([]);
+
+  const fetchResource = async resource => {
+    const responese = await axios.get(
+      `https://jsonplaceholder.typicode.com/${resource}`
+    );
+    setResources(responese.data);
+  };
+
+  useEffect(() => {
+    setResources([]);
+    fetchResource(resource);
+  }, [resource]);
+
+  if (!resources.length) {
+    return (
+      <div className="ui inverted segment">
+        <div classNam="ui inverted relaxed divided list">
+          <div class="ui segment">
+            <p></p>
+            <div class="ui active dimmer">
+              <div class="ui loader"></div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
   }
-}
+
+  return (
+    <div className="ui inverted segment">
+      <div classNam="ui inverted relaxed divided list">
+        {resources.map(post => (
+          <div className="item">
+            <div className="content">
+              <div className="header">
+                <a href="#">{post.title}</a>
+              </div>
+              <p>{resource === "posts" ? post.body : null}</p>
+            </div>
+            <hr />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default ResourceList;
